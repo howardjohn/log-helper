7035fe1abff904c5f22a1d9237ba977612930f3c
John Howard <howardjohn@google.com>
2021-01-04 09:56:35 -0800

    Add log highlighter

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/howardjohn/log-helper
 
 go 1.15
 
-require github.com/gookit/color v1.3.6
+require (
+	github.com/gookit/color v1.3.6
+	github.com/mkmik/argsort v1.1.0
+)
--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"regexp"
+	"time"
 
 	"github.com/gookit/color"
+	"github.com/mkmik/argsort"
 )
 
 type Matcher struct {
@@ -19,12 +23,26 @@ var (
 	colorTest = flag.Bool("test-colors", false, "test color support")
 )
 
+var (
+	knownLogFormats = []*regexp.Regexp{
+		regexp.MustCompile(`^20..-..-..T..:..:..\.......Z\t`),
+	}
+)
+
 func main() {
 	flag.Parse()
 	if *colorTest {
 		runColorTest()
 		return
 	}
+	all, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := logTimeBuffered(all); err != nil {
+		panic(err.Error())
+	}
+	return
 	matchers := []Matcher{}
 	for _, r := range flag.Args() {
 		rx := regexp.MustCompile(r)
@@ -43,6 +61,94 @@ func main() {
 		out := match(matchers, line)
 		w.Write([]byte(out))
 	}
+}
+
+type ParsedTime struct {
+	t    time.Time
+	bits int
+
+	delta time.Duration
+	rank  int
+}
+
+func matchTime(rs []*regexp.Regexp, data []byte) (*ParsedTime, error) {
+	for _, r := range rs {
+		if d := r.Find(data); d != nil {
+			t1, err := time.Parse(
+				`2006-01-02T15:04:05.999999`,
+				string(d)[:len(d)-2])
+			if err != nil {
+				return nil, err
+			}
+			return &ParsedTime{t: t1, bits: len(d) - 1}, nil
+		}
+	}
+	return nil, nil
+}
+
+type TimeSlice []*ParsedTime
+
+func (p TimeSlice) Len() int { return len(p) }
+func (p TimeSlice) Less(i, j int) bool {
+	if p[i] == nil {
+		return true
+	}
+	if p[j] == nil {
+		return false
+	}
+	return p[i].delta < p[j].delta
+}
+func (p TimeSlice) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+func logTimeBuffered(data []byte) error {
+	lines := bytes.Split(data, []byte("\n"))
+	times := make([]*ParsedTime, len(lines))
+	for i, line := range lines {
+		p, err := matchTime(knownLogFormats, line)
+		if err != nil {
+			return err
+		}
+		times[i] = p
+	}
+	lastTime := time.Time{}
+	for i := range lines {
+		p := times[i]
+		if p == nil {
+			continue
+		}
+		p.t.Sub(lastTime)
+		p.delta = p.t.Sub(lastTime)
+		lastTime = p.t
+	}
+
+	// todo blanks steal spots
+	ranks := argsort.Sort(TimeSlice(times))
+	for i, r := range ranks {
+
+		if times[r] != nil {
+			times[r].rank = i
+		}
+	}
+	for i := range lines {
+		p := times[i]
+		line := lines[i]
+		if p == nil {
+			fmt.Println(string(line))
+			continue
+		}
+
+		fmt.Printf("%s%s\n", rankToColor(p.rank, len(ranks)).Sprint(string(line[:p.bits])), string(line[p.bits:]))
+	}
+
+	return nil
+}
+
+func rankToColor(rank int, total int) color.RGBColor {
+	position := 1 - float64(rank)/float64(total)
+	if position <= 0.5 {
+		return color.RGB(255, uint8(255*position*2), 0)
+	}
+	return color.RGB(255-uint8(255*(position-0.5)*2), 255, 0)
 }
 
 func runColorTest() {
@@ -111,15 +217,11 @@ func runColorTest() {
 
 	fmt.Printf("\n%-50s24-bit Color\n", " ")
 	for i := 0; i < 256; i += 2 {
-		color.RGB(uint8(i), 0, 0, true).Printf(" ")
+		color.RGB(255, uint8(i), 0, true).Printf(" ")
 	}
 	fmt.Println()
-	for i := 0; i < 256; i += 2 {
-		color.RGB(0, uint8(i), 0, true).Printf(" ")
-	}
-	fmt.Println()
-	for i := 0; i < 256; i += 2 {
-		color.RGB(0, 0, uint8(i), true).Printf(" ")
+	for i := 255; i >= 0; i -= 2 {
+		color.RGB(255-uint8(i), 255, 0, true).Printf(" ")
 	}
 	fmt.Println()
 }
